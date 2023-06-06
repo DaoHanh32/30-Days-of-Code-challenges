520a3b3fd81b335ddd2484f2f5f968988f79bea9
DaoHanh32 <hanh32daovan@gmail.com>
2023-06-06 19:14:22 +0700

    fix typo

--- Day 4 - Class vs Instance/day 4 - class vs instance.cpp
+++ Day 4 - Class vs Instance/day 4 - class vs instance.cpp
@@ -22,7 +22,7 @@ class Person{
     void Person::amIOld(){
         // Do some computations in here and print out the correct statement to the console 
         if(age < 13) cout << "You are young." << endl;
-        else if(age >= 13 && age < 18) cout << "You are teenager." << endl;
+        else if(age >= 13 && age < 18) cout << "You are a teenager." << endl;
         else cout << "You are old." << endl;
     }
 
